4e196c06cb0c86dc45f920734a963fa8fb0ed35b
Roman <roman@ngknn.ru>
2025-11-22 09:12:47 +0300

    rebuild

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './app/router.jsx';
-import {ThemeProvider} from "./app/ThemeContext.js";
+import {ThemeProvider} from "./app/ThemeContext.tsx";
 
 
 createRoot(document.getElementById('root')).render(
